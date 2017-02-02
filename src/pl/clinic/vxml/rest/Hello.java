b6bef41fc0c9c25662c97d9bc62bf602d617d4dc
vandegor <adamos500@gmail.com>
2017-02-03 00:30:56 +0100

    podzial maina na subdialogi

--- src/pl/clinic/vxml/rest/Hello.java
+++ src/pl/clinic/vxml/rest/Hello.java
@@ -31,36 +31,76 @@ public class Hello {
 	@GET
 	@Produces(MediaType.TEXT_XML)
 	@Path("/main")
-	public Vxml sayPlainTextHello() throws MalformedURLException, JAXBException, SQLException {
+	public Vxml main() throws MalformedURLException, JAXBException, SQLException {
 		Vxml vxml = (Vxml) unmarshaller.unmarshal(new File(ProjectURL.getProjectURL("main.xml")));
-		Form form = vxml.getDataOrCatchOrHelp().stream()
-				.filter(o -> o instanceof Form && ((Form) o).getId().equals("action")).map(o -> (Form) o).findFirst()
-				.get();
 		return vxml;
+	}
+
+	@POST
+	@Path("/selectDoctor")
+	@Produces(MediaType.TEXT_XML)
+	@Consumes("application/x-www-form-urlencoded")
+	public Response selectDoctor(@FormParam("checkresults.patientId") Integer patientId)
+			throws MalformedURLException, JAXBException, SQLException {
+		Vxml vxml = (Vxml) unmarshaller.unmarshal(new File(ProjectURL.getProjectURL("selectDoctor.xml")));
+		return Response.ok(vxml, MediaType.TEXT_XML).build();
+	}
+
+	@POST
+	@Path("/selectTime")
+	@Produces(MediaType.TEXT_XML)
+	@Consumes("application/x-www-form-urlencoded")
+	public Response selectTime(@FormParam("checkresults.patientId") Integer patientId)
+			throws MalformedURLException, JAXBException, SQLException {
+		Vxml vxml = (Vxml) unmarshaller.unmarshal(new File(ProjectURL.getProjectURL("selectTime.xml")));
+		return Response.ok(vxml, MediaType.TEXT_XML).build();
+	}
+
+	@POST
+	@Path("/selectDate")
+	@Produces(MediaType.TEXT_XML)
+	@Consumes("application/x-www-form-urlencoded")
+	public Response selectDate(@FormParam("checkresults.patientId") Integer patientId)
+			throws MalformedURLException, JAXBException, SQLException {
+		Vxml vxml = (Vxml) unmarshaller.unmarshal(new File(ProjectURL.getProjectURL("selectDate.xml")));
+		return Response.ok(vxml, MediaType.TEXT_XML).build();
+	}
+
+	@POST
+	@Path("/newVisit")
+	@Produces(MediaType.TEXT_XML)
+	@Consumes("application/x-www-form-urlencoded")
+	public Response newVisit(@FormParam("checkresults.patientId") Integer patientId)
+			throws MalformedURLException, JAXBException, SQLException {
+		Vxml vxml = (Vxml) unmarshaller.unmarshal(new File(ProjectURL.getProjectURL("newVisit.xml")));
+		return Response.ok(vxml, MediaType.TEXT_XML).build();
+	}
+
+	@POST
+	@Path("/postponeVisit")
+	@Produces(MediaType.TEXT_XML)
+	@Consumes("application/x-www-form-urlencoded")
+	public Response postponeVisit(@FormParam("checkresults.patientId") Integer patientId)
+			throws MalformedURLException, JAXBException, SQLException {
+		Vxml vxml = (Vxml) unmarshaller.unmarshal(new File(ProjectURL.getProjectURL("postponeVisit.xml")));
+		return Response.ok(vxml, MediaType.TEXT_XML).build();
+	}
+
+	@POST
+	@Path("/cancelVisit")
+	@Produces(MediaType.TEXT_XML)
+	@Consumes("application/x-www-form-urlencoded")
+	public Response cancelVisit(@FormParam("checkresults.patientId") Integer patientId)
+			throws MalformedURLException, JAXBException, SQLException {
+		Vxml vxml = (Vxml) unmarshaller.unmarshal(new File(ProjectURL.getProjectURL("cancelVisit.xml")));
+		return Response.ok(vxml, MediaType.TEXT_XML).build();
 	}
 
 	@POST
 	@Path("/checkPass")
 	@Produces(MediaType.TEXT_XML)
 	@Consumes("application/x-www-form-urlencoded")
-	public Response sayXMLHello(@FormParam("patientNumber") String patientNumber,
-			@FormParam("patientPassword") String patientPassword)
-			throws MalformedURLException, JAXBException, SQLException {
-		Vxml vxml = (Vxml) unmarshaller.unmarshal(new File(ProjectURL.getProjectURL("subdialog.xml")));
-		// ObjectFactory factory = new ObjectFactory();
-		// Form form = factory.createForm("check");
-		// Block block = factory.createBlock();
-		// block.setName("patientNumberOK");
-		// form.getCatchOrHelpOrNoinput().add(block);
-		// vxml.getDataOrCatchOrHelp().add();
-		return Response.ok(vxml, MediaType.TEXT_XML).build();
-	}
-
-	@POST
-	@Path("/checkPass2")
-	@Produces(MediaType.TEXT_XML)
-	@Consumes("application/x-www-form-urlencoded")
-	public Response sayXMLHello2(@FormParam("patientNumber") Integer patientNumber,
+	public Response checkPass(@FormParam("patientNumber") Integer patientNumber,
 			@FormParam("patientPassword") Integer patientPassword)
 			throws MalformedURLException, JAXBException, SQLException {
 		Vxml vxml = (Vxml) unmarshaller.unmarshal(new File(ProjectURL.getProjectURL("checkAuth.xml")));
@@ -68,6 +108,7 @@ public class Hello {
 		Var response = (Var) form.getCatchOrHelpOrNoinput().get(0);
 		Var name = (Var) form.getCatchOrHelpOrNoinput().get(1);
 		Var surname = (Var) form.getCatchOrHelpOrNoinput().get(2);
+		Var patientId = (Var) form.getCatchOrHelpOrNoinput().get(3);
 
 		DaoUtils daoUtils = DaoUtils.getDaoUtils();
 		List<Patient> patients = daoUtils.getPatientDao().queryForEq("login", patientNumber);
@@ -75,6 +116,7 @@ public class Hello {
 			Patient patient = patients.get(0);
 			name.setExpr("'" + patient.getName() + "'");
 			surname.setExpr("'" + patient.getSurname() + "'");
+			patientId.setExpr("'" + patient.getId() + "'");
 			response.setExpr("'YES'");
 		} else {
 			response.setExpr("'NO'");
